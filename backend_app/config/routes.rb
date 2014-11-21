eb278234072a12d46254538ab393d2215b51e9b8
lawrencely <lawrence.ly667@gmail.com>
2014-11-21 15:01:10 +1100

    testing files for controllers

--- backend_app/config/routes.rb
+++ backend_app/config/routes.rb
@@ -1,65 +1,70 @@
 # == Route Map
 #
-# You don't have any routes defined!
-#
-# Please add some routes in config/routes.rb.
-#
-# For more information about routes, see the Rails guide: http://guides.rubyonrails.org/routing.html.
+#           Prefix Verb   URI Pattern                    Controller#Action
+#   category_index GET    /category(.:format)            category#index
+#                  POST   /category(.:format)            category#create
+#     new_category GET    /category/new(.:format)        category#new
+#    edit_category GET    /category/:id/edit(.:format)   category#edit
+#         category GET    /category/:id(.:format)        category#show
+#                  PATCH  /category/:id(.:format)        category#update
+#                  PUT    /category/:id(.:format)        category#update
+#                  DELETE /category/:id(.:format)        category#destroy
+#       user_index GET    /user(.:format)                user#index
+#                  POST   /user(.:format)                user#create
+#         new_user GET    /user/new(.:format)            user#new
+#        edit_user GET    /user/:id/edit(.:format)       user#edit
+#             user GET    /user/:id(.:format)            user#show
+#                  PATCH  /user/:id(.:format)            user#update
+#                  PUT    /user/:id(.:format)            user#update
+#                  DELETE /user/:id(.:format)            user#destroy
+#   customer_index GET    /customer(.:format)            customer#index
+#                  POST   /customer(.:format)            customer#create
+#     new_customer GET    /customer/new(.:format)        customer#new
+#    edit_customer GET    /customer/:id/edit(.:format)   customer#edit
+#         customer GET    /customer/:id(.:format)        customer#show
+#                  PATCH  /customer/:id(.:format)        customer#update
+#                  PUT    /customer/:id(.:format)        customer#update
+#                  DELETE /customer/:id(.:format)        customer#destroy
+#    product_index GET    /product(.:format)             product#index
+#                  POST   /product(.:format)             product#create
+#      new_product GET    /product/new(.:format)         product#new
+#     edit_product GET    /product/:id/edit(.:format)    product#edit
+#          product GET    /product/:id(.:format)         product#show
+#                  PATCH  /product/:id(.:format)         product#update
+#                  PUT    /product/:id(.:format)         product#update
+#                  DELETE /product/:id(.:format)         product#destroy
+#      order_index GET    /order(.:format)               order#index
+#                  POST   /order(.:format)               order#create
+#        new_order GET    /order/new(.:format)           order#new
+#       edit_order GET    /order/:id/edit(.:format)      order#edit
+#            order GET    /order/:id(.:format)           order#show
+#                  PATCH  /order/:id(.:format)           order#update
+#                  PUT    /order/:id(.:format)           order#update
+#                  DELETE /order/:id(.:format)           order#destroy
+# order_item_index GET    /order_item(.:format)          order_item#index
+#                  POST   /order_item(.:format)          order_item#create
+#   new_order_item GET    /order_item/new(.:format)      order_item#new
+#  edit_order_item GET    /order_item/:id/edit(.:format) order_item#edit
+#       order_item GET    /order_item/:id(.:format)      order_item#show
+#                  PATCH  /order_item/:id(.:format)      order_item#update
+#                  PUT    /order_item/:id(.:format)      order_item#update
+#                  DELETE /order_item/:id(.:format)      order_item#destroy
+#      store_index GET    /store(.:format)               store#index
+#                  POST   /store(.:format)               store#create
+#        new_store GET    /store/new(.:format)           store#new
+#       edit_store GET    /store/:id/edit(.:format)      store#edit
+#            store GET    /store/:id(.:format)           store#show
+#                  PATCH  /store/:id(.:format)           store#update
+#                  PUT    /store/:id(.:format)           store#update
+#                  DELETE /store/:id(.:format)           store#destroy
 #
 
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :category
+  resources :user
+  resources :customer
+  resources :product
+  resources :order
+  resources :order_item
+  resources :store
 end
---
+++ backend_app/spec/controllers/stores_controller_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe StoresController, :type => :controller do
+end
