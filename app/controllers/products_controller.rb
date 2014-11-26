9f25434dec2714df5ab4e6d32fa9e062979de42a
lawrencely <lawrence.ly667@gmail.com>
2014-11-26 15:47:29 +1100

    trying to add cross origin request cors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 #class Api::V1::ProductController < ApplicationController
 
 before_action :authenticate_api, :only => [:index, :show]
+after_action :access_control_headers
 
 # ajax request to get all products for index
 # $.ajax({
@@ -86,4 +87,9 @@ before_action :authenticate_api, :only => [:index, :show]
       @store = Store.where(api_key: token).first
     end
   end
+
+  def set_access_control_headers
+   headers['Access-Control-Allow-Origin'] = "*"
+   headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+ end
 end
