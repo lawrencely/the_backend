8d05bfab76e1d778115ea88499997e58dcad1b8c
lawrencely <lawrence.ly667@gmail.com>
2014-11-26 15:08:09 +1100

    changed pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   def home
     if @current_user.present?
       redirect_to stores_path(@current_user.stores.first)
+    else
+      redirect_to root_path
     end
   end
 end
