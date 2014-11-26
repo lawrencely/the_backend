2d1b68a7d7706e36c46be681773c6b65f79e9876
lawrencely <lawrence.ly667@gmail.com>
2014-11-26 15:10:34 +1100

    removed home html pages code for displaying stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
-    if @current_user.present?
-      redirect_to stores_path(@current_user.stores.first)
-    else
-      redirect_to root_path
-    end
+    # if @current_user.present?
+    #   redirect_to stores_path(@current_user.stores.first)
+    # else
+    #   redirect_to root_path
+    # end
   end
 end
