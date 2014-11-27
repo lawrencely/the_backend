0d7f5dad470af75b345ba88727c69c3955c358bc
lawrencely <lawrence.ly667@gmail.com>
2014-11-27 14:40:25 +1100

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,71 +13,131 @@ Order.destroy_all
 Product.destroy_all
 Category.destroy_all
 
-james = User.create(name: 'james lebron', email: 'jameslebron@jameslebron.com', password: 'jameslebron', options: '')
-james1 = User.create(name: 'james lebron1', email: 'jameslebron@jameslebron.com', password: 'jameslebron', options: '')
-james2 = User.create(name: 'james lebron2', email: 'jameslebron@jameslebron.com', password: 'jameslebron', options: '')
-james3 = User.create(name: 'james lebron3', email: 'jameslebron@jameslebron.com', password: 'jameslebron', options: '')
+lebron = User.create(name: 'lebron james', email: 'lebronjames@jameslebron.com', password: 'lebronjames', options: '')
 
-store = Store.create(user_id: 0, name: 'store', description: 'store ', api_key: '', api_secret: '')
+store = Store.create(user_id: 0, name: 'General Pants Co.', description: 'General Pants Co. is an Australian fashion retailer operating across NSW, VIC, QLD, SA and WA that sells both international and national brands. The retailer specialises in streetwear, denim, skate clothing and surfwear.', api_key: '', api_secret: '')
+
 store1 = Store.create(user_id: 0, name: 'store1', description: 'store 1', api_key: '', api_secret: '')
 store2 = Store.create(user_id: 0, name: 'store2', description: 'store 2', api_key: '', api_secret: '')
 store3 = Store.create(user_id: 0, name: 'store3', description: 'store 3', api_key: '', api_secret: '')
 
-customer = Customer.create(name: 'customer', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
-customer1 = Customer.create(name: 'customer1', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
-customer2 = Customer.create(name: 'customer2', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
-customer3 = Customer.create(name: 'customer3', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
+customer = Customer.create(name: 'Lawrence', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
+customer1 = Customer.create(name: 'James', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
+customer2 = Customer.create(name: 'Joel', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
+customer3 = Customer.create(name: 'Andrew', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
+customer4 = Customer.create(name: 'Rusli', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
+customer5 = Customer.create(name: 'Tanya', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
+customer6 = Customer.create(name: 'Jacqui', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
+customer7 = Customer.create(name: 'Angela', email: 'customer@email.com', password_digest: 'customer', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia')
+
 
 order = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
 order1 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
 order2 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
 order3 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
+order4 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
+order5 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
+order6 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
+order7 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
+order8 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
+order9 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
+order10 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
+order11 = Order.create(customer_id: 0, status: 'pending', memo: 'none', store_id: 0, street: '1 customer st', suburb: 'customer suburb', postcode: '1234', state: 'NSW', country: 'Australia', ip_address: 'none')
 
-category = Category.create(name: 'category', description: 'category_description', store_id: 0)
-category1 = Category.create(name: 'category1', description: 'category_description', store_id: 0)
-category2 = Category.create(name: 'category2', description: 'category_description', store_id: 0)
-category3 = Category.create(name: 'category3', description: 'category_description', store_id: 0)
+category = Category.create(name: 'Shirts', description: 'Shirt brands go here', store_id: 0)
+category1 = Category.create(name: 'Jeans', description: 'Jean brands go here', store_id: 0)
+category2 = Category.create(name: 'Jackets', description: 'Jacket brands go here', store_id: 0)
+category3 = Category.create(name: 'hats', description: 'Hat brand goes here', store_id: 0)
 
-product = Product.create(name: 'product', description: 'product_description', short_description: 'short_description', price: '123', product_attributes: '', image: '', store_id: 0)
-product1 = Product.create(name: 'product1', description: 'product_description', short_description: 'short_description', price: '123', product_attributes: '', image: '', store_id: 0)
-product2 = Product.create(name: 'product2', description: 'product_description', short_description: 'short_description', price: '123', product_attributes: '', image: '', store_id: 0)
-product3 = Product.create(name: 'product3', description: 'product_description', short_description: 'short_description', price: '123', product_attributes: '', image: '', store_id: 0)
+product = Product.create(name: 'T-Shirt', description: 'product_description', short_description: 'short_description', price: '20', product_attributes: '', image: '', store_id: 0)
+product1 = Product.create(name: 'V-Neck', description: 'product_description', short_description: 'short_description', price: '50', product_attributes: '', image: '', store_id: 0)
+product2 = Product.create(name: 'Scoop Neck', description: 'product_description', short_description: 'short_description', price: '30', product_attributes: '', image: '', store_id: 0)
+product3 = Product.create(name: 'Nudie Jeans', description: 'product_description', short_description: 'short_description', price: '60', product_attributes: '', image: '', store_id: 0)
+product4 = Product.create(name: 'Cheap Monday Jeans', description: 'product_description', short_description: 'short_description', price: '18', product_attributes: '', image: '', store_id: 0)
+product5 = Product.create(name: 'Hipster Hat', description: 'product_description', short_description: 'short_description', price: '25', product_attributes: '', image: '', store_id: 0)
+product6 = Product.create(name: 'Overcoat', description: 'product_description', short_description: 'short_description', price: '33', product_attributes: '', image: '', store_id: 0)
+product7 = Product.create(name: 'Cool Jacket', description: 'product_description', short_description: 'short_description', price: '64', product_attributes: '', image: '', store_id: 0)
+product8 = Product.create(name: 'Sunflower Hat', description: 'product_description', short_description: 'short_description', price: '12', product_attributes: '', image: '', store_id: 0)
+product9 = Product.create(name: 'Black Jeans', description: 'product_description', short_description: 'short_description', price: '93', product_attributes: '', image: '', store_id: 0)
+product10 = Product.create(name: 'Big Jacket', description: 'product_description', short_description: 'short_description', price: '99', product_attributes: '', image: '', store_id: 0)
+product11 = Product.create(name: 'Skinny Jeans', description: 'product_description', short_description: 'short_description', price: '150', product_attributes: '', image: '', store_id: 0)
 
-store.user_id = james.id
-store1.user_id = james1.id
-store2.user_id = james2.id
-store3.user_id = james3.id
+store.user_id = lebron.id
 
 customer.store_id = store.id
-customer1.store_id = store1.id
-customer2.store_id = store2.id
-customer3.store_id = store3.id
+customer1.store_id = store.id
+customer2.store_id = store.id
+customer3.store_id = store.id
+customer4.store_id = store.id
+customer5.store_id = store.id
+customer6.store_id = store.id
+customer7.store_id = store.id
+
 
 category.store_id = store.id
-category1.store_id = store1.id
-category2.store_id = store2.id
-category3.store_id = store3.id
+category1.store_id = store.id
+category2.store_id = store.id
+category3.store_id = store.id
 
-order.customer_id = customer.id
+order.customer_id = customer1.id
 order1.customer_id = customer1.id
 order2.customer_id = customer2.id
-order3.customer_id = customer3.id
+order3.customer_id = customer2.id
+order4.customer_id = customer2.id
+order5.customer_id = customer6.id
+order6.customer_id = customer6.id
+order7.customer_id = customer6.id
+order8.customer_id = customer5.id
+order9.customer_id = customer5.id
+order10.customer_id = customer5.id
+order11.customer_id = customer3.id
 
 order.store_id = store.id
 order1.store_id = store.id
 order2.store_id = store.id
 order3.store_id = store.id
+order4.store_id = store.id
+order5.store_id = store.id
+order6.store_id = store.id
+order7.store_id = store.id
+order8.store_id = store.id
+order9.store_id = store.id
+order10.store_id = store.id
+order11.store_id = store.id
+
 
 product.store_id = store.id
-product1.store_id = store1.id
-product2.store_id = store2.id
-product3.store_id = store3.id
+product1.store_id = store.id
+product2.store_id = store.id
+product3.store_id = store.id
+product4.store_id = store.id
+product5.store_id = store.id
+product6.store_id = store.id
+product7.store_id = store.id
+product8.store_id = store.id
+product9.store_id = store.id
+product10.store_id = store.id
+product11.store_id = store.id
 
+#category shirts
+category.products << product
+category.products << product1
+category.products << product2
+# category1 Jeans
+category1.products << product3
+category1.products << product4
+category1.products << product9
+category1.products << product11
 
-james.save
-james1.save
-james2.save
-james3.save
+# category2 jackets
+category2.products << product6
+category2.products << product7
+category2.products << product10
+# category3 hats
+category3.products << product5
+category3.products << product8
+
+lebron.save
 
 store.save
 store1.save
@@ -88,6 +148,10 @@ customer.save
 customer1.save
 customer2.save
 customer3.save
+customer4.save
+customer5.save
+customer6.save
+customer7.save
 
 category.save
 category1.save
@@ -98,15 +162,32 @@ product.save
 product1.save
 product2.save
 product3.save
+product4.save
+product5.save
+product6.save
+product7.save
+product8.save
+product9.save
+product10.save
+product11.save
 
 order.save
 order1.save
 order2.save
 order3.save
+order4.save
+order5.save
+order6.save
+order7.save
+order8.save
+order9.save
+order10.save
+order11.save
 
-
-
-
+category.save
+category1.save
+category2.save
+category3.save
 
 
 
