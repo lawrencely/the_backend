7179adb669059f5d06680236eed826788bf1a770
lawrencely <lawrence.ly667@gmail.com>
2014-12-10 14:38:20 +1100

    changing routes

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -38,34 +38,33 @@ RSpec.describe CustomersController, :type => :controller do
 
   describe 'CREATE' do
     before do
-    @current_user = User.create!(
-      name: 'test',
-      email: 'test@test.com',
-      password: 'testtest',
-      password_confirmation: 'testtest'
-    )
-    @store = Store.create!(
-      name: 'test_store',
-      description: 'test_store_desciption',
-      api_key: '',
-      api_secret: '',
-      user_id: @current_user.id
-    )
-    @customer_valid = {
-      name: 'test',
-      email: 'test@test.com',
-      password: 'testtest',
-      store_id: @store.id,
-    }
+      @current_user = User.create!(
+        name: 'test',
+        email: 'test@test.com',
+        password: 'testtest',
+        password_confirmation: 'testtest'
+      )
+      @store = Store.create!(
+        name: 'test_store',
+        description: 'test_store_desciption',
+        api_key: '',
+        api_secret: '',
+        user_id: @current_user.id
+      )
+      @customer_valid = {
+        name: 'test',
+        email: 'test@test.com',
+        password: 'testtest',
+        store_id: @store.id,
+      }
 
-    @customer_invalid = {
-      name: 'tsdf',
-      email: 'test@test.com',
-      password: '',
-      store_id: @store.id
-    }
+      @customer_invalid = {
+        name: 'tsdf',
+        email: 'test@test.com',
+        password: '',
+        store_id: @store.id
+      }
       @request.env['HTTP_AUTHORIZATION'] = "Token token=#{@store.api_key}"
-
     end
 
     context 'post with valid information' do
@@ -98,90 +97,27 @@ RSpec.describe CustomersController, :type => :controller do
         expect(parsed_response['error'].length).to eql 1
       end
     end
-
-    # describe 'UPDATE' do
-    #   before do
-    #     @customer_one = Customer.create!(
-    #       name: 'test',
-    #       email: 'test@test.com',
-    #       password: 'testtest',
-    #       store_id: @store.id
-    #     )
-    #     updated_customer = {
-    #       name: 'testtest',
-    #       email: 'test@test.com',
-    #       password: 'testtest',
-    #       store_id: @store.id
-    #     }
-    #       request.env['HTTP_AUTHORIZATION'] = "Token token=#{@store.api_key}"
-    #       put :update, { id: @customer_one.id, customer: updated_customer }, {'Authorization' => "Token token=#{@store.api_key}" }
-    #   end
-
-    #   # it 'should update customer' do
-    #   #   binding.pry
-    #   #   expect(@customer.name).to eq('testtest')
-    #   # end
-    # end
   end
+  describe 'UPDATE' do
+    before do
+      @customer = Customer.create!(
+        name: 'test',
+        email: 'test@test.com',
+        password: 'testtest',
+        store_id: @store.id
+      )
+      updated_customer = {
+        name: 'testtest123',
+        email: 'test@test.com',
+        password: 'testtest',
+        store_id: @store.id
+      }
+        request.env['HTTP_AUTHORIZATION'] = "Token token=#{@store.api_key}"
+        put :update, { id: @customer.id, customer: updated_customer }, {'Authorization' => "Token token=#{@store.api_key}" }
+    end
 
-#xhr :put, :update, :format => "js"
-
-
-  # describe 'Index' do
-  #   before do
-  #     @current_user = User.create!(
-  #       name: 'test',
-  #       email: 'test@test.com',
-  #       password: 'testtest',
-  #       password_confirmation: 'testtest'
-  #     )
-  #     @store = Store.create!(
-  #       name: 'test_store',
-  #       description: 'test_store_desciption',
-  #       api_key: '',
-  #       api_secret: '',
-  #       user_id: @current_user.id
-  #       )
-  #     @customer = Customer.create!(
-  #     name: 'test',
-  #     email: 'test@test.com',
-  #     password: 'testtest'
-  #       )
-  #     @store.customers << @customer
-  #     get :index, nil, { user_id: @current_user.id}
-  #   end
-  #   it 'should assign customers variable' do
-  #     expect(assigns(:customers)).to eq(@store.customers)
-  #   end
-  # end
-
-  #   @category = Category.create!(
-  #     name: 'test_category',
-  #     description: 'test_category_desciption',
-  #     store_id: @store.id
-  #   )
-  #   @product = Product.create!(
-  #     name: 'test_product',
-  #     description: 'test_description',
-  #     short_description: 'test_short_description',
-  #     price: 'test_price',
-  #     product_attributes: 'test_product_attributes',
-  #     store_id: @store.id,
-  #     image: 'test_image',
-  #     category_ids: [@category.id]
-  #   )
-  #   @customer = {
-  #     name: 'test',
-  #     email: 'test@test.com'
-  #   }
-  #   post :create, {customer: @customer }, { }
-  # end
-  # describe 'receiving JSON and saving in database' do
-  #   describe 'CREATE POST' do
-  #     it 'should take JSON and save to database' do
-
-  #     end
-  #   end
-  # end
-
+    it 'should update customer' do
+      expect(@customer.name).to eq('testtest123')
+    end
+  end
 end
